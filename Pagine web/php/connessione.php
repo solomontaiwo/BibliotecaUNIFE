e2390fe53286429cf7b8dc2d38f399fda51684a6
solomontaiwo <solomonolamide.taiwo@edu.unife.it>
2023-07-12 16:39:18 +0200

    Aggiornamento credenziali connessione

--- Pagine web/php/connessione.php
+++ Pagine web/php/connessione.php
@@ -1,5 +1,5 @@
 <?php
-	$link = mysqli_connect("127.0.0.1", "solomon", "solomon", "biblioteca");
+	$link = mysqli_connect("127.0.0.1", "solomon", "solomon123", "BibliotecaUNIFE");
 
 	if (!$link) { 
 		echo "Impossibile collegarsi al database. Per cambiare le credenziali modifica il file /php/connessione.php";
