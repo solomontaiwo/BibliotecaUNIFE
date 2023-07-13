04cb15ab48ce43fcd0be934eed866af10cb99a68
solomontaiwo <solomonolamide.taiwo@edu.unife.it>
2023-07-13 18:28:47 +0200

    Correzione finale (si spera) posizionamento cartelel

--- php/connessione.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$link = mysqli_connect("127.0.0.1", "solomon", "solomon123", "BibliotecaUNIFE");
-
-	if (!$link) { 
-		echo "Impossibile collegarsi al database. Per cambiare le credenziali modifica il file /php/connessione.php";
-		exit(-1);
-	}
-?>
